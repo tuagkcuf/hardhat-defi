eb7f65c2602dd810d692acfd80e3e1d88701ce2a
tuagkcuf <jokersms222@gmail.com>
2023-04-04 16:23:17 +0300

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        localhost: {
+            chaindId: 31337,
+        },
+        hardhat: {
+            chaindId: 31337,
+            blockConfirmations: "1",
+        },
+        sepolia: {
+            chaindId: 11155111,
+            blockConfirmations: "6",
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            saveDeployments: true,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    solidity: "0.8.18",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 500000,
+    },
+}
