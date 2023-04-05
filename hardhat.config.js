5629707d44043766ab7949ac342aff89c9c2eece
tuagkcuf <jokersms222@gmail.com>
2023-04-05 14:39:33 +0300

    add weth script

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,7 @@ const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "https://mainnet"
 
 module.exports = {
     defaultNetwork: "hardhat",
